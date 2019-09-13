d4696f77922c213a54ec3c3a3d4dfa31a1c426db
kuehnd96 <kuehnd96@gmail.com>
2019-09-13 15:45:51 -0500

    Implement list of questions.

---
+++ Week3/qa-app/frontend/src/Questions/Questions.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Questions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: null,
+    };
+  }
+
+  async componentDidMount() {
+    const questions = (await axios.get('http://localhost:8081/')).data;
+    this.setState({
+      questions,
+    });
+  }
+
+  render() {
+    // The binding that checks for null questions in state will render before a response is returned by axios
+    //  The second binding will render when questions in populated in this.state
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.questions === null && <p>Loading questions...</p>}
+          {
+            this.state.questions && this.state.questions.map(question => (
+              <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
+                <Link to={`/question/${question.id}`}>
+                  <div className="card text-white bg-success mb-3">
+                    <div className="card-header">Answers: {question.answers}</div>
+                    <div className="card-body">
+                      <h4 className="card-title">{question.title}</h4>
+                      <p className="card-text">{question.description}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Questions;

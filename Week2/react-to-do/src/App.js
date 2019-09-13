ec06e26a0319931d98b4530e80b1137e87ee3458
kuehnd96 <kuehnd96@gmail.com>
2019-09-13 10:38:41 -0500

    Added ability to add todo items.

--- Week2/react-to-do/src/App.js
+++ Week2/react-to-do/src/App.js
@@ -10,6 +10,12 @@ function App() {
     {text: "Build really cool todo app"}
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
@@ -20,11 +26,41 @@ function App() {
             todo={todo} 
           />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
 }
 
+// How we display a todo item
 const Todo = ({ todo }) => <div className="todo">{todo.text}</div>
+
+// Input form for new todo items
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    addTodo(value);
+
+    setValue("");
+  };
+
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 export default App;

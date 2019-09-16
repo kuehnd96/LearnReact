65be11b9dceceb0c1ef9931021392aa9075e4abe
kuehnd96 <kuehnd96@gmail.com>
2019-09-16 12:46:12 -0500

    Prevent check for user until silent auth is done.

--- Week3/qa-app/frontend/src/SecuredRoute/SecuredRoute.js
+++ Week3/qa-app/frontend/src/SecuredRoute/SecuredRoute.js
@@ -3,10 +3,11 @@ import {Route} from 'react-router-dom';
 import auth0Client from '../Auth';
 
 function SecuredRoute(props) {
-  const {component: Component, path} = props;
+  const {component: Component, path, checkingSession} = props;
   return (
     <Route path={path} render={() => {
-        if (!auth0Client.isAuthenticated()) {
+      if (checkingSession) return <h3 className="text-center">Validating session...</h3>;  
+      if (!auth0Client.isAuthenticated()) {
           auth0Client.signIn();
           return <div></div>;
         }

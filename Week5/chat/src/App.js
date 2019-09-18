9a32fb2bd8cce1c9ea0ef4d09ba7dec732de0a50
kuehnd96 <kuehnd96@gmail.com>
2019-09-18 16:25:35 -0500

    Creating the root component

--- Week5/chat/src/App.js
+++ Week5/chat/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+const DUMMY_DATA = [
+  {
+      senderId: "perbogen",
+      text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages: DUMMY_DATA
+    };
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <MessageList messages={this.state.messages} />
+        <SendMessageForm />
+      </div>
+    )
+  }
 }
 
 export default App;

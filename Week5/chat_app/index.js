e3554da7f1787f4444362d1b242429af0c59c04f
kuehnd96 <kuehnd96@gmail.com>
2019-09-18 15:25:52 -0500

    Creation of App component & dummy data.

---
+++ Week5/chat_app/index.js
@@ -0,0 +1,31 @@
+const DUMMY_DATA = [
+    {
+        senderId: "perborgen",
+        text: "who'll win?"
+    },
+    {
+        senderId: "janedoe",
+        text: "who'll win?"
+    }
+]
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            messages: DUMMY_DATA
+        }
+    }
+    
+    render () {
+        return (
+            <div className="app">
+                <Title />
+                <MessageList messages={this.state.messages} />
+                <SendMessageForm />
+            </div>
+        )
+    }
+}

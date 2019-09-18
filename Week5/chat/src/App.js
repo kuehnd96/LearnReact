11eb5958a09261589ec18c9e460b4918f36f4cd4
kuehnd96 <kuehnd96@gmail.com>
2019-09-18 16:52:37 -0500

    Step 5: Fetching API-keys from Chatkit.

--- Week5/chat/src/App.js
+++ Week5/chat/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import MessageList from "./MessageList"
 
+/*
+
+instance locator: v1:us1:58358383-107a-400c-9ae9-bbd142203629
+test token provider: https://us1.pusherplatform.io/services/chatkit_token_provider/v1/58358383-107a-400c-9ae9-bbd142203629/token
+Room id: 69cd8b0b-8353-4aeb-a103-1731beccb35f
+username: perbogen
+
+*/
+
 const DUMMY_DATA = [
   {
       senderId: "perbogen",

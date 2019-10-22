b35ab3af53c406d1cddda060e226ae57418ea5ed
kuehnd96 <kuehnd96@gmail.com>
2019-10-22 06:34:04 -0500

    Calendar Functionality

--- Week7/react-calendar/src/components/Calendar.jsx
+++ Week7/react-calendar/src/components/Calendar.jsx
@@ -2,12 +2,29 @@ import React from "react"
 import dateFns from "date-fns"
 
 class Calendar extends React.Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date()
+  }
+
+  renderHeader() {}
+
+  renderDays() {}
+
+  renderCells() {}
+
+  onDateClick = day => {}
+
+  nextMonth = () => {}
+
+  prevMonth = () => {}
+  
   render() {
       return (
-        <div>
-            <div>Header</div>
-            <div>Days</div>
-            <div>Cells</div>
+        <div className="calendar">
+            {this.renderHeader()}
+            {this.renderDays()}
+            {this.renderCells()}
         </div>
       );
   }

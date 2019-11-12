0ed5ff2c4c13832639aab7844366e14e4a6a5d99
kuehnd96 <kuehnd96@gmail.com>
2019-11-12 14:26:06 -0600

    Week 14: 0:13:45 to 0:25:35

--- Week14/resorts/src/App.js
+++ Week14/resorts/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+// Instructions: https://www.youtube.com/watch?v=ScDWrogElmo&feature=youtu.be
 
 function App() {
   return (
     <>
-    Resorts!
+      <Home/>
+      <Rooms/>
+      <SingleRoom/>
+      <Error/>
     </>
   );
 }

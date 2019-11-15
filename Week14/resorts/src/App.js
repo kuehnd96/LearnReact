81eb605efa9d5cf8fc8ffa1ee56032004622bb0f
kuehnd96 <kuehnd96@gmail.com>
2019-11-15 15:41:23 -0600

    Week 14: 0:38:00 to 1:00:00

--- Week14/resorts/src/App.js
+++ Week14/resorts/src/App.js
@@ -6,12 +6,14 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 // Instructions: https://www.youtube.com/watch?v=ScDWrogElmo&feature=youtu.be
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />

d061a7c7f8076a4446aafd0b94d52d8dddb95ae3
kuehnd96 <kuehnd96@gmail.com>
2019-11-18 15:31:32 -0600

    Week 14: 1:00:00 to 2:25:30

---
+++ Week14/resorts/src/components/FeaturedRooms.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import {RoomContext} from '../context'
+import Title from './Title'
+import Loading from './Loading'
+import Room from './Room'
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext
+    
+    render() {
+        let { loading, featuredRooms: rooms} = this.context
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+
+        return (
+            <div>
+                <section className="featured-rooms">
+                    <Title title="Featured Rooms"/>
+                    <div className="featured-rooms-centered">
+                        {loading?<Loading/>:rooms}
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
